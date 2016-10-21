e8123ce43de3966adcfc5fe1eaecc31633042205
Alex Musayev <alex.musayev@gmail.com>
2016-10-21 14:57:37 +0300

    Rails project dependencies and configuration

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require bootstrap.min
+//= require jquery-ui/core
+//= require jquery-ui/effect-highlight
 //= require_tree .
